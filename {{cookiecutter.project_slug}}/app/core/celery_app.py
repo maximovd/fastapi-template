41286ece3050f2c0be92c39d43fe966c0561eeef
Danil Maximov <d.maksimov@zebrains.team>
2022-01-30 22:59:02 +0300

    Update and clean project slug config

--- {{cookiecutter.project_slug}}/app/core/celery_app.py
+++ {{cookiecutter.project_slug}}/app/core/celery_app.py
@@ -1,5 +1,7 @@
 from celery import Celery
 
-celery_app = Celery("worker", broker="amqp://guest@queue//")
+from app.core.config import settings
+
+celery_app = Celery(settings.CELERY_APP_NAME, broker=settings.CELERY_BROKER)
 
 celery_app.conf.task_routes = {"app.worker.test_celery": "main-queue"}
