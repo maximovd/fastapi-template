0b782046fbf67847fc03d91f1c4784dbf888fec4
Danil Maximov <d.maksimov@zebrains.team>
2022-01-30 23:18:11 +0300

    update main.py

--- {{cookiecutter.project_slug}}/app/main.py
+++ {{cookiecutter.project_slug}}/app/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
@@ -15,3 +16,6 @@ if settings.BACKEND_CORS_ORIGINGS:
         allow_methods=["*"],
         allow_headers=["*"],
     )
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
