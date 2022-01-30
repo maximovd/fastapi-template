6187a380ee4fc7dfd8d3c2b1fb49f12b7d4e1f50
Danil Maximov <d.maksimov@zebrains.team>
2022-01-30 23:42:05 +0300

    Add base api skeleton

---
+++ {{cookiecutter.project_slug}}/app/api/endpoints/utils.py
@@ -0,0 +1,15 @@
+from typing import Any
+
+from fastapi import APIRouter
+
+from app import models, schemas
+from app.core.celery_app import celery_app
+
+router = APIRouter()
+
+
+@router.post("/test-celery/", response_model=schemas.Msg, status_code=201)
+def test_celery(msg: schemas.Msg) -> Any:
+    """Test Celery worker."""
+    celery_app.send_task("app.worker.test_celery", args=[msg.msg])
+    return {"msg": "Message received"}
