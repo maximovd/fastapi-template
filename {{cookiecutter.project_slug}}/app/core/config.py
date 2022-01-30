71be9c81efdee39e3b317b1cd60c8463e93be1e7
Danil Maximov <d.maksimov@zebrains.team>
2022-01-30 23:17:31 +0300

    update slug project config, add .env.example

--- {{cookiecutter.project_slug}}/app/core/config.py
+++ {{cookiecutter.project_slug}}/app/core/config.py
@@ -1,14 +1,13 @@
 import secrets
 from typing import Any, Dict, List, Optional, Union
 
-from pydantic import AnyHttpUrl, BaseSettings, EmailStr, HttpUrl, PostgresDsn, validator
+from pydantic import AnyHttpUrl, BaseSettings, PostgresDsn, validator
 
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     SECRET_KEY: str = secrets.token_urlsafe(32)
     SERVER_NAME: str
-    SERVER_HOST: AnyHttpUrl
 
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
 
