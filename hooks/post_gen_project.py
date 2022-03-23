9ccbe56134839d04aad846f4a8a44a4c71357a6e
Danil Maximov <bazinga.mail@yandex.ru>
2022-03-23 22:49:34 +0300

    ✨ add creating .env

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,6 +6,14 @@ def choice_package_manger():
     else:
         os.remove("pyproject.toml")
 
+def extract_env_variables():
+    with open(".env.example", "r") as example_file, open(".env", "a") as target_file:
+        for line in example_file:
+            target_file.write(line)
+ 
 
 if __name__ == "__main__":
     choice_package_manger()
+    if os.path.isfile(".env.example"):
+        extract_env_variables()
+
