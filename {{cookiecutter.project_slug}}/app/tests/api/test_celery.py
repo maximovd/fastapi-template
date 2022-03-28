585949c52de6bb45835a266bbba69d1cee4b5a1b
Danil Maximov <d.maksimov@zebrains.team>
2022-03-28 23:31:31 +0300

    ✅ update test

--- {{cookiecutter.project_slug}}/app/tests/api/test_celery.py
+++ {{cookiecutter.project_slug}}/app/tests/api/test_celery.py
@@ -5,8 +5,8 @@ from fastapi.testclient import TestClient
 from app.core.config import settings
 
 
-def test_celery_worker_test(client: TestClient, superuser_token_headers: Dict[str, str]) -> None:
+def test_celery_worker_test(client: TestClient) -> None:
     data = {"msg": "test"}
-    r = client.post(f"{settings.API_V1_STR}/utils/test-celery/", json=data, headers=superuser_token_headers)
+    r = client.post(f"/utils/test-celery/", json=data)
     response = r.json()
-    assert response["msg"] == "Word received"
+    assert response["msg"] == "Message received"
