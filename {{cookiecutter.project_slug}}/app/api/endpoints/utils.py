b5b76fa15e2ab5073b497eb8fe7affdc2fb59e16
Danil Maximov <d.maksimov@zebrains.team>
2022-03-28 21:22:16 +0300

    🐛 fix celery tasks

--- {{cookiecutter.project_slug}}/app/api/endpoints/utils.py
+++ {{cookiecutter.project_slug}}/app/api/endpoints/utils.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter
 
 from app import schemas
 from app.core.celery_app import celery_app
+from app.worker import test_celery_task
 
 router = APIRouter()
 
@@ -11,5 +12,5 @@ router = APIRouter()
 @router.post("/test-celery/", response_model=schemas.Msg, status_code=201)
 def test_celery(msg: schemas.Msg) -> Any:
     """Test Celery worker."""
-    celery_app.send_task("app.worker.test_celery", args=[msg.msg])
+    test_celery_task.delay(msg.msg)
     return {"msg": "Message received"}
--- {{cookiecutter.project_slug}}/app/worker.py
+++ {{cookiecutter.project_slug}}/app/worker.py
@@ -2,5 +2,5 @@ from app.core.celery_app import celery_app
 
 
 @celery_app.task(acks_late=True)
-def test_celery(msg: str) -> str:
+def test_celery_task(msg: str) -> str:
     return f"test task return {msg}"
