758df666c19ba0e98493aedf6f63918cc518a52d
Danil Maximov <d.maksimov@zebrains.team>
2022-03-28 20:49:19 +0300

    🔧 update cookiecutter fiels

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 def choice_package_manger():
     if "{{ cookiecutter.package_manager }}".lower() == "poetry":
@@ -12,10 +13,18 @@ def extract_env_variables():
     with open(".env.example", "r") as example_file, open(".env", "a") as target_file:
         for line in example_file:
             target_file.write(line)
- 
+
+def fix_endlines_for_sh_files():
+    path: Path
+    for path in Path(".").glob("**/*.sh"):
+        lf_data = path.read_bytes().replace(b"\r\n", b"\n")
+        path.write_bytes(lf_data)
+    makefile = Path(".", "Makefile")
+    lf_data = makefile.read_bytes().replace(b"\r\n", b"\n")
+    makefile.write_bytes(lf_data)
 
 if __name__ == "__main__":
     choice_package_manger()
+    fix_endlines_for_sh_files()
     if os.path.isfile(".env.example"):
         extract_env_variables()
-
